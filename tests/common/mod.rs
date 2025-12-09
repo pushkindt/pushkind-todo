4cc7547371b3098307355a6125b50745869e370a
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-12-09 22:37:31 +0300

    Refactor TestDb to use temporary files for integration tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,42 +1,34 @@
 //! Helpers for integration tests.
 
 use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
-
 use pushkind_common::db::{DbPool, establish_connection_pool};
+use tempfile::NamedTempFile;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!(); // assumes migrations/ exists
 
 /// Temporary database used in integration tests.
 pub struct TestDb {
-    filename: String,
+    _tempfile: NamedTempFile,
     pool: DbPool,
 }
 
 impl TestDb {
-    pub fn new(filename: &str) -> Self {
-        std::fs::remove_file(filename).ok(); // Clean up old DB
-
-        let pool =
-            establish_connection_pool(filename).expect("Failed to establish SQLite connection.");
+    pub fn new() -> Self {
+        let tempfile = NamedTempFile::new().expect("Failed to create temp file");
+        let pool = establish_connection_pool(tempfile.path().to_str().unwrap())
+            .expect("Failed to establish SQLite connection.");
         let mut conn = pool
             .get()
             .expect("Failed to get SQLite connection from pool.");
         conn.run_pending_migrations(MIGRATIONS)
             .expect("Migrations failed");
         TestDb {
-            filename: filename.to_string(),
+            _tempfile: tempfile,
             pool,
         }
     }
+
     pub fn pool(&self) -> DbPool {
         self.pool.clone()
-    }
-}
-
-impl Drop for TestDb {
-    fn drop(&mut self) {
-        std::fs::remove_file(&self.filename).ok();
-        std::fs::remove_file(format!("{}-shm", &self.filename)).ok();
-        std::fs::remove_file(format!("{}-wal", &self.filename)).ok();
     }
 }
